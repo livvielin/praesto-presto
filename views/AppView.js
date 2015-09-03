b51b1521ea4231a0e843875da1f6b043d74f7799
Hack Reactor Students <accounts@hackreactor.com>
2015-09-03 16:43:20 -0700

    Re-renders AppView when user presses enter key in the form

--- views/AppView.js
+++ views/AppView.js
@@ -2,22 +2,36 @@
 
 var AppView = Backbone.View.extend({
 
-  // var headerHTML: '<h1>Awesome To Do List</h1>',
-  // var formHTML: '<form><input type="text" name="entry" placeholder="What do you want to do?"></form>',
-  // var footerHTML: '<div>Copyright 2015</div>',
+  headerHTML: '<h1>Awesome To Do List</h1>',
+
+  formHTML: '<form><input type="text" name="entry" placeholder="What do you want to do?"></form>',
+  
+  footerHTML: '<div>Copyright 2015</div>',
+  
   el: 'body',
 
+  events: {
+    'keypress input': function(e) {
+      if (e.which == 13) {
+        e.preventDefault();
+        this.render();
+      }
+    }
+  },
+
   render: function() {
+    this.$el.children().detach();
     // header html
-    this.$el.html('<h1>Awesome To Do List</h1>');
+    this.$el.append(this.headerHTML);
 
     // html form
-
+    this.$el.append(this.formHTML);
 
     // listentryviews
 
 
     // footer
+    this.$el.append(this.footerHTML);
 
   }
 
